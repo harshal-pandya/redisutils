a187bb1ae1bb28495e5bf040406d2ea217485a9d
Harshal Pandya <harshalgem@gmail.com>
2013-02-19 16:42:21 -0500

    added export method

--- src/main/scala/com/github/harshal/MapStats.scala
+++ src/main/scala/com/github/harshal/MapStats.scala
@@ -2,13 +2,14 @@ package com.github.harshal
 
 import redis.clients.jedis.Jedis
 import collection.JavaConverters._
+import java.io.PrintWriter
 
 /**
  * Methods to get statistics from a [String,Double] map
  * @author harshal
  * @date: 2/18/13
  */
-class MapStats(host:String , port:Int ) extends Jedis(host:String , port:Int){
+class RedisUtils(host:String , port:Int ) extends Jedis(host:String , port:Int){
 
   /**
    * Return count of keys for a given value (Inverse map)
@@ -34,22 +35,41 @@ class MapStats(host:String , port:Int ) extends Jedis(host:String , port:Int){
     temp.groupBy(_._1).map(kv=>kv._1->kv._2.map(_._2))
   }
 
+  def inverseMap:Map[Double,Set[String]] = {
+    readToMem.groupBy(_._2).map(kv=>kv._1->kv._2.map(_._1).toSet)
+  }
+
   def readToMem:Map[String,Double]={
     val keySet = keys("*").asScala
     keySet.map(key=> key -> get(key).toDouble).toMap
   }
 
+  def export(file:java.io.File,del:String="\t"){
+    val writer = new PrintWriter(file)
+    val keySet = keys("*").asScala
+    try{
+      for (key<-keySet){
+        writer.write(key+del+get(key)+"\n")
+      }
+    }catch{
+      case e:Exception => println(e.getStackTraceString)
+    }finally {
+      writer.close()
+    }
+  }
+
 }
 
-object MapStats{
+object RedisUtils{
   def main(args:Array[String])={
     assert(args.length==2,"Please pass host and port!!")
     val host = args(0).trim
     val port = args(1).toInt
-    val mapStats = new MapStats(host,port)
+    val utils = new RedisUtils(host,port)
 
-    val res = mapStats.inverseMap(Set(1.0,2.0,3.0))
+    val res = utils.inverseMap(Set(1.0,2.0,3.0))
     println("Stats")
+    println("Size : "+utils.dbSize())
     res.foreach(m=>println(m._1+" : "+m._2.size))
   }
 }
